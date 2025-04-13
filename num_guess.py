ae215224923ade59a035e7dee7df2d5bad79335d
Patrick-eng7 <kanepa@oregonstate.edu>
2025-04-13 11:47:08 -0700

    Add files via upload

--- num_guess.py
+++ num_guess.py
@@ -1,13 +1,19 @@
+# Ask the user to enter the target number
 target_number = int(input("Enter the integer for the player to guess.\n"))
 
+# Initialize guess count
 guess_count = 0
 correct_guess = False
 
+# Loop until the player guesses correctly
 while not correct_guess:
-
+    # Ask the player to enter their guess
     guess = int(input("Enter your guess.\n"))
 
+    # Increment the guess count
     guess_count += 1
+
+    # Check if the guess is too high, too low, or correct
     if guess > target_number:
         print("Too high - try again:")
     elif guess < target_number:
