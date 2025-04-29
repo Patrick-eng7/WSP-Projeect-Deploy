6fb6ae70a560847901c7ba0ee153c58ed9334a9c
Patrick-eng7 <kanepa@oregonstate.edu>
2025-04-29 14:56:43 -0700

    Add files via upload

--- add_surname.py
+++ add_surname.py
@@ -6,23 +6,9 @@
 #              with the letter "K" using list comprehension.
 
 
+
+
 def add_surname(names):
-    """Return a new list with ' Smith' added to each name."""
-    return [name + " Smith" for name in names]
+    return [name + " Kardashian" for name in names if name.startswith('K')]
 
-
-def find_median(numbers):
-    """Return the median of a list of numbers."""
-    sorted_nums = sorted(numbers)
-    n = len(sorted_nums)
-    mid = n // 2
-    if n % 2 == 0:
-        return (sorted_nums[mid - 1] + sorted_nums[mid]) / 2
-    else:
-        return sorted_nums[mid]
-
-
-def has_duplicates(items):
-    """Return True if the list contains duplicate values, otherwise False."""
-    return len(items) != len(set(items))
 
